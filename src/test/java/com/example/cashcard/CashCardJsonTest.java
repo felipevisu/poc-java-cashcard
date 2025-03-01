8363cb034f4839287c22ccdee8c8987c1a70d090
Felipe Faria <felipevisu@gmail.com>
2025-03-01 11:37:03 -0300

    Save json into a variable

--- src/test/java/com/example/cashcard/CashCardJsonTest.java
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -17,10 +17,11 @@ public class CashCardJsonTest {
     @Test
     void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
-        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
-        assertThat(json.write(cashCard)).hasJsonPathNumberValue(("@.id"));
-        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
-        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
-        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+        var jsonContent = json.write(cashCard);
+        assertThat(jsonContent).isStrictlyEqualToJson("expected.json");
+        assertThat(jsonContent).hasJsonPathNumberValue(("@.id"));
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(jsonContent).hasJsonPathNumberValue("@.amount");
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
     }
 }
